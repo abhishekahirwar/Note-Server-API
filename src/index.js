d772117617f2e68f8c7bb02377ea3cdd1a4615c7
Abhishek Ahirwar <abhishekahirwar1289@gmail.com>
2023-07-21 01:29:50 +0530

    Notes Server

---
+++ src/index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+
+const mongoose = require('mongoose');
+const Note = require('./models/Note');
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+mongoose.connect('mongodb+srv://innocentboyabhi1234:bekarpasswordHaibhai@cluster1.juycsvr.mongodb.net/notesdb').then(function () {
+    app.get('/', function (req, res) {
+        const response = { message: 'API Works!' };
+        res.json(response);
+    });
+
+    const noteRouter = require('./routes/Note');
+    app.use('/notes', noteRouter);
+
+});
+
+// Starting the server on a PORT
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, function () {
+    console.log('Server started at PORT: ' + PORT);
+});
+
